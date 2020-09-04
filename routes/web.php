ab5ac2b3db65bd1c7839e6f618cf54df81c4482b
AlexeyMalitskiy <web.alexey.malitskiy@gmail.com>
2020-09-04 17:43:33 +0300

    rout group admin

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,14 @@ Route::get('/', function () {
 });
 
 Route::post('/registration', 'RegistrationController@createRegistration');
-Route::get('/admin', 'Admin\AdminController@index');
-Route::get('/admin/blog', 'Admin\AdminBlogController@index')->name('admin-blog');
-Route::get('/admin/blog/create', 'Admin\AdminBlogController@create')->name('admin-blog-create');
-Route::post('/admin/blog/store', 'Admin\AdminBlogController@store')->name('admin-blog-store');
-Route::get('/admin/blog/edit/{id}', 'Admin\AdminBlogController@edit')->name('admin-blog-edit');
-Route::post('/admin/blog/update/', 'Admin\AdminBlogController@update')->name('admin-blog-update');
-//Route::get('/admin/blog/show/', 'Admin\AdminBlogController@show')->name('admin-blog-show');
-Route::get('/admin/blog/show/{param}', 'Admin\AdminBlogController@show')->name('admin-blog-show');
-Route::get('/admin/blog/delite/{id}', 'Admin\AdminBlogController@destroy')->name('admin-blog-delite');
+
+Route::group([ 'prefix' => 'admin' ], function ($router) {
+    Route::get('/', 'Admin\AdminController@index');
+    Route::get('/blog', 'Admin\AdminBlogController@index')->name('admin-blog');
+    Route::get('/blog/create', 'Admin\AdminBlogController@create')->name('admin-blog-create');
+    Route::post('/blog/store', 'Admin\AdminBlogController@store')->name('admin-blog-store');
+    Route::get('/blog/edit/{id}', 'Admin\AdminBlogController@edit')->name('admin-blog-edit');
+    Route::post('/blog/update/', 'Admin\AdminBlogController@update')->name('admin-blog-update');
+    Route::get('/blog/show/{param}', 'Admin\AdminBlogController@show')->name('admin-blog-show');
+    Route::get('/blog/delite/{id}', 'Admin\AdminBlogController@destroy')->name('admin-blog-delite');
+});
