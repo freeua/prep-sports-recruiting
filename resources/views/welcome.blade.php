3406da4335f80d4e283f4f7c844fbc9a0050fee2
AlexeyMalitskiy <web.alexey.malitskiy@gmail.com>
2020-09-08 13:00:36 +0300

    merged branch origin PSR-005.Blog-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,7 @@
     </head>
     <body>
         <div id="root"></div>
+        <div assetPath="{{asset ('/storage/')}}"></div>
 
     <script type="text/javascript" src="/js/index.js"></script>
         <script type="text/javascript">
