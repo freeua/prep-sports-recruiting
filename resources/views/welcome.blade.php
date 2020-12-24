df05a06aa460f1d72c233b79735f5241233a10ce
Hurricane0 <nikitadv777@gmail.com>
2020-12-24 10:29:39 +0200

    Backend changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Prep Sports</title>
 
         <!-- Fonts -->
 {{--        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">--}}
 {{--        <link href="/css/app.css" rel="stylesheet" type="text/css">--}}
-
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
     </head>
     <body>
         <div id="root"></div>
@@ -18,6 +21,7 @@
     <script type="text/javascript" src="/js/index.js"></script>
         <script type="text/javascript">
             window.token = '{{ csrf_token() }}';
+            window.assetPath = "{{asset ('/storage/')}}"
         </script>
     </body>
 </html>
