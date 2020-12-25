b2812e295a3c828de77a50962734410ebf5b65e9
Hurricane0 <nikitadv777@gmail.com>
2020-12-25 08:56:32 +0200

    Changed frontend routes to api

--- resources/js/PrepSports/PrepSports/src/api/blog.api.js
+++ resources/js/PrepSports/PrepSports/src/api/blog.api.js
@@ -1,15 +1,15 @@
 import post from "./services/post";
 import get from "./services/get";
 
-export const getBlogs = () => post("/blog/articles");
+export const getBlogs = () => post("/api/blog/articles");
 
-export const getSidebarArticles = () => get("/blog/articles-sidebar");
+export const getSidebarArticles = () => get("/api/blog/articles-sidebar");
 
 // prev_next = "1" - if next, "0" - if prev
 export const getNextPosts = (lastPostId, prev_next) =>
-  post("/blog/articles-pagination", {
+  post("/api/blog/articles-pagination", {
     id: lastPostId,
     prev_next
   });
 
-export const getSinglePost = id => post("/blog/article", { id });
+export const getSinglePost = id => post("/api/blog/article", { id });
